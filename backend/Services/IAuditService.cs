abade86ba93d2ddd815f5d1694bd001a059b4401
R-JDiaz <diazrj54@gmail.com>
2026-03-20 09:20:27 +0800

    fix some resp

--- backend/Services/IAuditService.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Http;
-
-namespace VotingSystemBackend.Services
-{
-    public interface IAuditService
-    {
-        Task LogActionAsync(string action, string entityType, string entityId, HttpRequest request);
-    }
-}
--- backend/Services/IEncryptionService.cs
+++
@@ -1,9 +0,0 @@
-namespace VotingSystemBackend.Services
-{
-    public interface IEncryptionService
-    {
-        string Hash(string value);
-        bool Verify(string value, string hash);
-        string GenerateToken(int length = 32);
-    }
-}
