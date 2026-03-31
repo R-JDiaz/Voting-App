ab89163aa2402928f9d738af53d046da87de6529
R-JDiaz <diazrj54@gmail.com>
2026-04-01 01:08:18 +0800

    entry point listen in port(dependent in env)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
+const BACKEND_PORT = process.env.BACKEND_PORT;
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -10,6 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(BACKEND_PORT, () => {
+  console.log(`Server is running on http://localhost:${BACKEND_PORT}`);
 });
