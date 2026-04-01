6391688e7a603536a3b1bc607bcf208abc6a77e4
R-JDiaz <diazrj54@gmail.com>
2026-04-01 19:42:16 +0800

    model: election

--- backend/src/models/election.js
+++ backend/src/models/election.js
@@ -1,7 +1,49 @@
-require('dotenv').config();
-
-const mysql = require('mysql2/promise');
-const { master_pool, slave_pool, slave_pool } = require('../config/db');
+const { master_pool, slave_pool } = require('../config/db');
 
 const master_db = master_pool;
 const slave_db = slave_pool;
+
+const Election = {
+    async getAll() {
+        const [rows]  = await slave_db.query('SELECT * FROM elections');
+        return rows;
+    },
+
+    async getById(id) {
+        const [rows] = await slave_db.query(
+            'SELECT * FROM elections WHERE id = ?',
+            [id]
+        );
+        return rows[0];
+    },
+
+    async create(data) {
+        const { title, description, start_date, end_date, status } = data;
+
+        const [result] = await master_pool.query(
+            `INSERT INTO election (title, description, start_date, end_date, status)
+             VALUES (?, ?, ?, ?, ?)`,
+            [title, description, start_date, end_date, status]
+        );
+
+        return result.insertId;
+    },
+
+    async update(id, data) {
+        const { title, description, start_date, end_date, status } = data;
+        
+        await master_db.query(
+            `UPDATE elections 
+            SET title=?, description=?, start_date=?, end_date=?, status=?
+            WHERE id=?`,
+            [title, description, start_date, end_date, status, id]
+        );
+    },
+
+    async delete(id) {
+        await master_db.query('DELETE FROM elecions WHERE id=?',
+        [id]);
+    }
+};
+
+module.exports = Election;
