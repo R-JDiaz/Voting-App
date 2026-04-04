122a36f92613323ce21d8f8f671d09fa8068fbb0
R-JDiaz <diazrj54@gmail.com>
2026-04-04 23:00:53 +0800

    wrapper for controllers

--- backend/src/controllers/election.js
+++ backend/src/controllers/election.js
@@ -2,61 +2,41 @@ import ElectionService from "../services/election.js";
 
 const ElectionController = {
     async getAll(req, res, next) {
-        try {
-            const elections = await ElectionService.getAll();
-            res.status(200).json(elections);
-        } catch (err) {
-            next(err);
-        }
+        const elections = await ElectionService.getAll();
+        res.status(200).json(elections);
     },
 
     async getById(req, res, next) {
-        try {
-            const { id } = req.params;
+        const { id } = req.params;
 
-            const election = await ElectionService.getById(id);
+        const election = await ElectionService.getById(id);
 
-            res.status(200).json(election);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(election);
     },
 
     async create(req, res, next) {
-        try {
-            const election = await ElectionService.create(req.body);
+        const election = await ElectionService.create(req.body);
 
-            res.status(201).json({
-                message: "Election created successfully",
-                data: election
-            });
-        } catch (err) {
-            next(err);
-        }
+        res.status(201).json({
+            message: "Election created successfully",
+            data: election
+        });
     },
 
     async update(req, res, next) {
-        try {
-            const { id } = req.params;
+        const { id } = req.params;
 
-            const result = await ElectionService.update(id, req.body);
+        const result = await ElectionService.update(id, req.body);
 
-            res.status(200).json(result);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(result);
     },
 
     async delete(req, res, next) {
-        try {
-            const { id } = req.params;
+        const { id } = req.params;
 
-            const result = await ElectionService.delete(id);
+        const result = await ElectionService.delete(id);
 
-            res.status(200).json(result);
-        } catch (err) {
-            next(err);
-        }
+        res.status(200).json(result);
     }
 };
 
