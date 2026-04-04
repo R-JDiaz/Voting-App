d41f6d268961b6adcdb040d652e51c473d5714da
R-JDiaz <diazrj54@gmail.com>
2026-04-05 01:55:32 +0800

    change to named export

--- backend/src/utils/auth/token_generator.js
+++ backend/src/utils/auth/token_generator.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (userId) => jwt.sign(
+export const generateToken = (userId) => jwt.sign(
     { id: userId },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
 );
-
-export default generateToken;
