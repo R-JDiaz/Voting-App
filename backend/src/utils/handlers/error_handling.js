bf63bbd6f54efaa673c791081b3bb8c3a9a4728f
R-JDiaz <diazrj54@gmail.com>
2026-04-07 19:00:10 +0800

    updt to handle code message

--- backend/src/utils/handlers/error_handling.js
+++ backend/src/utils/handlers/error_handling.js
@@ -1,6 +1,7 @@
 export default class AppError extends Error {
-    constructor(message, statusCode = 400) {
+    constructor(message, status=400, code) {
         super(message);
-        this.statusCode = statusCode;
+        this.code = code;
+        this.status = status;
     }
 }
