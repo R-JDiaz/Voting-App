b62a565c28f7ea743a192eef38fe6c8149852998
R-JDiaz <diazrj54@gmail.com>
2026-04-07 19:11:20 +0800

    add the validate middleware

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,12 @@
 import AuthController from "../controllers/auth.js";
 import { asyncHandler } from "../utils/handlers/async_handler.js";
 import { Router } from "express";
+import { loginSchema, registerSchema } from "../schemas/auth.js";
+import { validate } from "../middlewares/validate.js";
 
 const router = Router();
 
-router.post("/register", asyncHandler(AuthController.register));
-router.post("/login", asyncHandler(AuthController.login));
+router.post("/register",validate(registerSchema), asyncHandler(AuthController.register));
+router.post("/login", validate(loginSchema), asyncHandler(AuthController.login));
 
 export default router;
