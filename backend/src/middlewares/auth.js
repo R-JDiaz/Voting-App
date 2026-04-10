bc0a4f73961bff7dec800ce38ae343dcf8939211
R-JDiaz <diazrj54@gmail.com>
2026-04-10 21:50:26 +0800

    feat: refactor authorization middleware to improve user and role checks

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,3 +1,4 @@
+import { AppError } from '../utils/handlers/response_handler';
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -23,3 +24,47 @@ const authenticateToken = (req, res, next) => {
     next();
   });
 };
+
+const isUser = (req) => {
+  if (!req.user) {
+      throw new AppError(
+        'Request User are Required', 
+        401,
+        'REQUEST_USER_ARE_EMPTY'
+    );
+  }
+}
+
+const isAllowedRole = (req, iterable) => {
+  if (!iterable.includes(req.user.role)) {
+      throw new AppError(
+        'Insufficient Permission',
+        403,
+        'INSUFFICIENT_PERMISSION'
+    );
+  }
+}
+export const authorizeRole = (...allowedRoles) => {
+  return (req, res, next) => {
+    isUser(req);
+    isAllowedRole(req, allowedRoles);
+    next();
+  };
+};
+
+export const authorizePermission = (...allowedRoles) => {
+  return (req, res, next) => {
+    
+    isUser(req);
+    isAllowedRole(req, allowedRoles);
+    if (!req.user.can_create_election) {
+      throw new AppError(
+        'User doesnt have sufficient permission',
+        403,
+        'INSUFFICIENT_PERMISSION'
+      );
+    }
+
+    next();
+  }
+}
