02ab381ae779afbec216a777351b1f863eb8263b
R-JDiaz <diazrj54@gmail.com>
2026-04-10 23:10:02 +0800

    feat: add role-based authorization to admin and auth routes

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -1,21 +1,50 @@
 import AdminController from "../controllers/admin.js";
 import { asyncHandler } from "../utils/handlers/async_handler.js";
 import { Router } from "express";
+import { authorizeRole } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/", asyncHandler(AdminController.getAll));
-router.get("/:id", asyncHandler(AdminController.getById));
-router.get("/username/:username", asyncHandler(AdminController.getByUsername));
+router.get(
+  "/",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.getAll)
+);
 
-router.post("/", asyncHandler(AdminController.create));
-router.put("/:id", asyncHandler(AdminController.update));
+router.get(
+  "/:id",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.getById)
+);
+
+router.get(
+  "/username/:username",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.getByUsername)
+);
+
+router.post(
+  "/",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.create)
+);
+
+router.put(
+  "/:id",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.update)
+);
 
 router.put(
   "/password/:id",
+  authorizeRole(["ADMIN"]),
   asyncHandler(AdminController.updatePassword)
 );
 
-router.delete("/:id", asyncHandler(AdminController.delete));
+router.delete(
+  "/:id",
+  authorizeRole(["ADMIN"]),
+  asyncHandler(AdminController.delete)
+);
 
 export default router;
