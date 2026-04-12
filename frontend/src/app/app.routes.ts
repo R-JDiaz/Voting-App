160c36a6f3e159fa8c42e35488810dd0d0907159
R-JDiaz <diazrj54@gmail.com>
2026-04-13 00:50:06 +0800

    fix: correct routing configuration for fallback to login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,5 +9,8 @@ export const routes: Routes = [
     { path: 'login', component: Login},
     { path: 'home', 
       component: Home,
-      canActivate: [ authGuard ] }
+      canActivate: [ authGuard ] },
+    { path: '**', 
+      redirectTo: 'login', 
+      pathMatch: 'full' }
 ];
