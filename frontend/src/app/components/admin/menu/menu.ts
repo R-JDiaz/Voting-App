12876c8a32366e47f44c9b55ccda863d4847f6fa
R-JDiaz <diazrj54@gmail.com>
2026-04-14 23:25:45 +0800

    feat: refactor admin menu layout and integrate user components

--- frontend/src/app/components/admin/menu/menu.ts
+++ frontend/src/app/components/admin/menu/menu.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environments';
+import { UserList } from './user-list/user-list';
+import { Navbar } from '../../navbar/navbar';
+import { UserBox } from './user-box/user-box';
 
 @Component({
   selector: 'app-admin-menu',
-  imports: [],
+  imports: [UserList, Navbar, UserBox],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
-export class AdminMenu {}
+export class AdminMenu {
+}
